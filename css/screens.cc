15544cde7acff6d7251587282a079493ecd95445
NoahV050 <ikbenbeter0508@gmail.com>
2026-03-18 10:24:35 +0100

    Bezig met het maken van de overgangen in de schermen

---
+++ css/screens.cc
@@ -0,0 +1,12 @@
+/* Alle schermen verborgen by default */
+.screen {
+  display: none;
+}
+
+/* Alleen het actieve scherm zichtbaar */
+.screen.active {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
